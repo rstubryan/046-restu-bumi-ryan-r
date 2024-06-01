7402e1d90db738e8977b6ffee14e3deac936e621
rstubryan <restubumirr@gmail.com>
2024-06-01 22:13:24 +0700

    feat(+): add pages and static routing, not using models-controller yet

--- resources/js/Pages/CMS.jsx
+++ resources/js/Pages/CMS.jsx
@@ -1,18 +1,63 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/Components/ui/table";
 
 export default function Dashboard({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">CMS</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    CMS
+                </h2>
+            }
         >
             <Head title="CMS" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">Hello World!</div>
+                        <Table>
+                            <TableCaption>
+                                A list of your recent invoices.
+                            </TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">
+                                        Product ID
+                                    </TableHead>
+                                    <TableHead>Name</TableHead>
+                                    <TableHead>Categories</TableHead>
+                                    <TableHead>Amount</TableHead>
+                                    <TableHead className="text-right">
+                                        Action
+                                    </TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell className="font-medium">
+                                        INV001
+                                    </TableCell>
+                                    <TableCell>Slicing Design</TableCell>
+                                    <TableCell>Front-End</TableCell>
+                                    <TableCell>$250.00</TableCell>
+                                    <TableCell className="text-right">
+                                        <button className="text-blue-600">
+                                            Edit
+                                        </button>
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
+Route::get('/dashboard', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
@@ -15,11 +15,19 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
+Route::get('/dashboard/admin', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard/cms', [CMSController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard/cms');
+Route::get('/dashboard/admin/cms', [CMSController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard/cms');
+
+Route::get('/', function () {
+    return Inertia::render('Content/index');
+});
+
+Route::get('/checkout', function () {
+    return Inertia::render('Form/index');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

8ab1832dd988d0ee56c24e0d72e672a73df2d7d6
rstubryan <restubumirr@gmail.com>
2024-06-01 22:07:33 +0700

    feat(components/organisms): add footer

---
+++ resources/js/Components/Content/organisms/Footer/index.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <>
+            <div className="bg-primary w-full text-center mt-10">
+                <p className="text-white py-5">
+                    &copy;2024 - Restu Bumi Ryan Ramadhan
+                </p>
+            </div>
+        </>
+    );
+}
